ce18594a8c0ab3e8fce7a56ade309459359933d5
Matthew Morris <ms20mgpm@cetus.cs.ox.ac.uk>
2021-08-13 11:47:35 +0100

    Link ctp files. Set working directory

--- run.sh
+++ run.sh
@@ -12,13 +12,11 @@ if hash nvidia-docker 2>/dev/null; then
     shift
     NV_GPU=${gpu} nvidia-docker run -ti --rm \
             --net host \
-            -v /data/${hs}/${USER}/:/users/${USER}/data/ \
-            -v /users/${USER}/BAIML:/users/${USER}/BAIML \
-            -v /users/${USER}/rl_duq:/users/${USER}/rl_duq \
-            -v /users/${USER}/vate:/users/${USER}/vate \
-            --env PYTHONPATH="/users/${USER}/BAIML:/users/${USER}/rl_duq:/users/${USER}/rl_duq/gpduq:/users/${USER}/vate" \
+            -v /users/${USER}/ctp:/users/${USER}/ctp \
+	    -w /users/${USER}/ctp \
+            --env PYTHONPATH="/users/${USER}/ctp" \
             ${USER}/${dockerfile} \
-            $@
+	    $@
 else
     cpuset=$1
     shift
